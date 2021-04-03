27e34ddb54b76fd391bb8cc80e0040b19565bd7b
riabalvas <riabalvas@gmail.com>
2021-04-03 20:31:04 +0300

    next change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -122,7 +122,7 @@
           
           
     </div>
-    <!-- <script>
+    <script>
  id=1;
  for(id = 1; id < 101; id++){
 a=document.getElementById(id).innerHTML;
@@ -155,7 +155,7 @@ c = c + f[i];
 document.getElementById(id).innerHTML=c;
 
 }
-</script> -->
+</script>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
      
